3c81b0c94bdf4b3d8a50d5e9103386b3b3cac26f
Hector Martinez III <hmartineziii@gmail.com>
2015-05-18 20:06:48 -0700

    initial commit added express and static site with beginnings of a nav bar

---
+++ server.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(port, function(){
+	console.log("starting server at port: " + port)
+});
