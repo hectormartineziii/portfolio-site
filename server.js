7e1b2b5c520d307b0a3aa4f8e7c63e642ef8bd49
Hector Martinez III <hmartineziii@gmail.com>
2015-07-26 10:08:27 -0700

    added blog entry #2

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/blog', express.static(path.join(__dirname,'blog')));
 app.use('/blog/1', express.static(path.join(__dirname,'blog/1')))
+app.use('/blog/2', express.static(path.join(__dirname,'blog/2')))
 
 app.listen(port, function(){
 	console.log("starting server at port: " + port)
