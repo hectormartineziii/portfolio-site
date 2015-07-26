31a074c4238ec6704ce646aa7b5e3027643e7c23
Hector Martinez III <hmartineziii@gmail.com>
2015-07-26 11:16:44 -0700

    new entry on READMEs

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/blog', express.static(path.join(__dirname,'blog')));
 app.use('/blog/1', express.static(path.join(__dirname,'blog/1')))
 app.use('/blog/2', express.static(path.join(__dirname,'blog/2')))
+app.use('/blog/3', express.static(path.join(__dirname,'blog/3')))
 
 app.listen(port, function(){
 	console.log("starting server at port: " + port)
