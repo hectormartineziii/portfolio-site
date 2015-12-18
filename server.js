56e68c8c7ab1c8698b6ba5ed706a15926cea6f90
Hector Martinez III <hmartineziii@gmail.com>
2015-12-17 22:14:59 -0800

    added blog entry 4

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get('/blog', function(req,res){res.render('../blog/index',{})});
 app.get('/blog/1', function(req,res){res.render('../blog/1/index',{})});
 app.get('/blog/2', function(req,res){res.render('../blog/2/index',{})});
 app.get('/blog/3', function(req,res){res.render('../blog/3/index',{})});
+app.get('/blog/4', function(req,res){res.render('../blog/4/index',{})});
 
 
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -26,6 +27,7 @@ app.use('/blog', express.static(path.join(__dirname,'public')));
 app.use('/blog/1', express.static(path.join(__dirname,'public')));
 app.use('/blog/2', express.static(path.join(__dirname,'public')));
 app.use('/blog/3', express.static(path.join(__dirname,'public')));
+app.use('/blog/4', express.static(path.join(__dirname,'public')));
 
 app.listen(port, function(){
 	console.log("starting server at port: " + port)
