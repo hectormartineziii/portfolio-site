c309d28551b480d3291c53895bb2a102a6d8704f
Hector Martinez <hmartineziii@gmail.com>
2017-11-09 08:33:34 -0600

    added a fifth blog for a class project serving a large CPG firm

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.get('/blog/1', function(req,res){res.render('../blog/1/index',{})});
 app.get('/blog/2', function(req,res){res.render('../blog/2/index',{})});
 app.get('/blog/3', function(req,res){res.render('../blog/3/index',{})});
 app.get('/blog/4', function(req,res){res.render('../blog/4/index',{})});
+app.get('/blog/5', function(req,res){res.render('../blog/5/index',{})});
 
 
 app.use('/', express.static(path.join(__dirname, 'public')));
@@ -28,6 +29,7 @@ app.use('/blog/1', express.static(path.join(__dirname,'public')));
 app.use('/blog/2', express.static(path.join(__dirname,'public')));
 app.use('/blog/3', express.static(path.join(__dirname,'public')));
 app.use('/blog/4', express.static(path.join(__dirname,'public')));
+app.use('/blog/5', express.static(path.join(__dirname,'public')));
 
 app.listen(port, function(){
 	console.log("starting server at port: " + port)
